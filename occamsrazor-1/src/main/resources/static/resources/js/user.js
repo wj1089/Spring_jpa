4e8ae814dd42d9398a2e8569fd49826b21a4f67b
wj1089 <wj1089@naver.com>
2020-05-01 16:29:38 +0900

    0501    16:30

---
+++ occamsrazor-1/src/main/resources/static/resources/js/user.js
@@ -0,0 +1,13 @@
+"use strict"
+var user = user || {} //네임스페이스, {} = Member mem = "null";
+user =(()=>{//=>람다까지 달린다!
+	let init =()=>{} //초기화함수, 
+	let onCreate =() =>{//on만들어지자마자라는 뜻
+		
+	}
+	let setContentViews =() =>{
+		
+	}
+	return {setContentViews}//즉시실행
+})()
+
